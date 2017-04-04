887f77229281683e42ed320aa51204b8fb052650
Eugene <eugene.komissarov@gmail.com>
2017-04-04 18:09:53 +0300

    Create 4_range_object.py

---
+++ 4_range_object.py
@@ -0,0 +1,15 @@
+#https://docs.python.org/3/tutorial/controlflow.html
+a=range(0,10,3)
+print(a, type(a))
+for i in a:
+    print(i)
+
+print('range не \'одноразовый\'',a)
+
+#использование range() для иттерации по индексам последовательности
+a = ['Mary', 'had', 'a', 'little', 'lamb']
+for i in range(len(a)):
+    print(i, a[i])
+    
+#список может быть создан на основе иттерируемого объекта класса range
+print(list(range(5)))
