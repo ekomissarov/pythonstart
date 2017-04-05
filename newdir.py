9fac38e7eb25e199d446686a57578917b7468b57
Eugene <eugene.komissarov@gmail.com>
2017-04-05 18:26:09 +0300

    Update newdir.py

--- newdir.py
+++ newdir.py
@@ -3,7 +3,7 @@
 dirList={id(eval(i)):list() for i in dir()}
 for i in dir():
     if id(eval(i)) in dirList.keys():
-        dirList[id(eval(i))].append(i)
+        dirList[id(eval(i))].append(i) #eval интерпретирует строку как команду Python в текущем пространстве имен
 
 print("{")
 for i in dirList: print(i,dirList[i],sep=': ')
