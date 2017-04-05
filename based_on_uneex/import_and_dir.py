b5f6382cb86215998aa45aa3eb23145af0676a01
Eugene <eugene.komissarov@gmail.com>
2017-04-05 18:45:40 +0300

    Create import_and_dir.py

---
+++ based_on_uneex/import_and_dir.py
@@ -0,0 +1,11 @@
+#[UNИX][Python] Лекция 3. Стандартные типы данных и выражения-конструкторы
+from math import *
+
+print(dir(),'\n\n')
+
+a=[1,2,3,3.14]
+b=map(sin,a)
+for i in b:
+    print(i)
+    
+print(b)
