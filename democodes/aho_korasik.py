58a7f58b7103044b65f7e5b1d2c2d59238012f6d
Eugene <eugene.komissarov@gmail.com>
2017-04-05 17:27:30 +0300

    Update aho_korasik.py

--- democodes/aho_korasik.py
+++ democodes/aho_korasik.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 #http://ru.stackoverflow.com/questions/383679/%D0%9F%D0%BE%D0%B8%D1%81%D0%BA-%D0%BF%D0%BE%D0%B2%D1%82%D0%BE%D1%80%D1%8F%D1%8E%D1%89%D0%B8%D1%85%D1%81%D1%8F-%D1%81%D1%82%D1%80%D0%BE%D0%BA
+#https://ru.wikipedia.org/wiki/%D0%90%D0%BB%D0%B3%D0%BE%D1%80%D0%B8%D1%82%D0%BC_%D0%90%D1%85%D0%BE_%E2%80%94_%D0%9A%D0%BE%D1%80%D0%B0%D1%81%D0%B8%D0%BA
 #Ахо-Корасик-подобный алгоритм
 import re # сопоставление с шаблонами, регулярные выражения
 import sys # предоставляет доступ к компонентам системного окружения
