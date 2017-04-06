66e85bebed1fcb2395847d3a4c716f64c4696233
Eugene <eugene.komissarov@gmail.com>
2017-04-06 14:21:06 +0300

    Create tuple.py

---
+++ based_on_uneex/tuple.py
@@ -0,0 +1,23 @@
+#[UNИX][Python] Лекция 3. Стандартные типы данных и выражения-конструкторы
+from math import sin
+
+a,b,c = 1,2,3
+print('(1,2,3) тип:', type((1,2,3)))
+
+z=(1,2,'sfas',[1,2,3,4],sin)
+print(z)
+print(dir(z))
+print(z[4])
+print('\n\n')
+
+s=((1,2),(3,4),(5,6))
+for i in s: print(i)
+for i,j in s: print(i,' * ',j, ' = ', i*j)
+print('\n\n')
+
+a=(1,1,32,215,56,43,65,75,756,7)
+print(a)
+print(a[3:6])
+print(a[3::2])
+print(a[9:2:-1])
+print(id(a),id(a[:]), a is a[:]) #копия - одно и то же т.к. список не изменяемый
