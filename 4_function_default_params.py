9ffdba638daf8cfcc756c077445c5ebe1bf2d580
Eugene <eugene.komissarov@gmail.com>
2017-04-06 17:30:23 +0300

    Update 4_function_default_params.py

--- 4_function_default_params.py
+++ 4_function_default_params.py
@@ -6,6 +6,10 @@ def ask_ok(prompt, retries=4, reminder='Please try again!'):
         оператор in проверяет членство элемента слева, во множестве справа
         not in - отсутствие во множестве
         is / is not - проверка объектов на идентичность (id() - идунтификатор объекта)
+        методы из класса:
+            __contains__
+            __iter__
+            __getitem__
         '''
         if ok in ('y', 'ye', 'yes'):
             return True
