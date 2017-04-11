ffdd8ba8904e4e5b3e13dc3ae8f675422d7763b0
Eugene <eugene.komissarov@gmail.com>
2017-04-11 16:59:36 +0300

    Create 5_transposed_list.py

---
+++ 5_transposed_list.py
@@ -0,0 +1,9 @@
+#https://docs.python.org/3/tutorial/datastructures.html#nested-list-comprehensions
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+]
+print(matrix)
+a = [[row[i] for row in matrix] for i in range(4)]
+print(a)
