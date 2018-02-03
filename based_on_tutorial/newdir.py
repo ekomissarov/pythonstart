c4ed6fab3c3018c089bde52106a37b59ff327203
eugene <eugene.komissarov@gmail.com>
2018-02-03 16:43:58 +0300

    upd1

---
+++ based_on_tutorial/newdir.py
@@ -0,0 +1,10 @@
+# учебный скрипт ===
+# Выводит результат dir() в виде словаря {id, лист названий}, уникализация по id
+dirList={id(eval(i)):list() for i in dir()}
+for i in dir():
+    if id(eval(i)) in dirList.keys():
+        dirList[id(eval(i))].append(i) #eval интерпретирует строку как команду Python в текущем пространстве имен
+
+print("{")
+for i in dirList: print(i,dirList[i],sep=': ')
+print("}")
