fd3b79441c9f3a83be10b38a8369f6782c53d1b3
eugene <eugene.komissarov@gmail.com>
2018-02-18 20:31:28 +0300

    upd1

---
+++ uneex_homework/4_2.py
@@ -0,0 +1,45 @@
+'''
+матрицы, вложенные структуры
+'''
+
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+]
+
+print([[row[i] for row in matrix] for i in range(len(matrix[0]))])
+
+# оно же более подробно
+transposed = []
+for i in range(len(matrix[0])):  # цикл по столбцам
+    transposed.append([row[i] for row in matrix])  # цикл по строкам
+print(transposed)
+
+
+# оно же еще подробнее
+transposed = []
+for i in range(len(matrix[0])):  # цикл по столбцам
+    transposed_raw = []
+    for raw in matrix:  # для каждого i-ого столбца разворачиваем строки
+        transposed_raw.append(raw[i])
+    transposed.append(transposed_raw)
+
+print(transposed)
+
+#  с помошью функции zip
+transposed = list(zip(*matrix))
+print(transposed)
+
+#  по другому с предварителььной распаковкой
+a, *b = matrix
+print(tuple(zip(a, *b)))
+
+
+# пример использования del и
+a = [i**2 for i in range(10)]
+print(a)
+del a[3:4] # удалит 3-ий элемент (это полуинтервал)
+# удаление приводит к переиндексированию массива, см результат вывода массива
+for k,v in enumerate(a):  # enumerate позволяет бесплатно получить сразу ключ, значение
+    print(k,v)
