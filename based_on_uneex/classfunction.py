66e7cbbe19a1989e0dbe4d605437b7c0d11d43d1
eugene <eugene.komissarov@gmail.com>
2018-03-01 17:20:37 +0300

    upd1

--- based_on_uneex/classfunction.py
+++ based_on_uneex/classfunction.py
@@ -1,10 +1,9 @@
 from modul import printlist
 
-print("\n\nCLASS C")
+print("\n\nCLASS C илюстрация к bound method передача параметра self")
 
 class C:
     pass
-
 
 def fun(*par):
     print(par)
@@ -22,7 +21,12 @@ b.foon("asdfg")  # внезапно у метода класса есть еще
 # это сделано чтобы можно было обращаться к своим собственным полям
 
 
-print("\n\nCLASS B")
+
+
+
+
+
+print("\n\nCLASS B илюстрация к пространству имен")
 class B:
     var = 100500
     def fun(self, a):
@@ -33,6 +37,11 @@ b = B()
 print(*(s for s in dir(b) if not s.startswith("_")))
 print(b.fun(33))
 print(*(s for s in dir(b) if not s.startswith("_")))  # образовалось еще одно поле Another
+
+B.newAnother="one"
+print("класс:", *(s for s in dir(B) if not s.startswith("_")))  # образовалось еще одно поле newAnother
+print("экземпляр:", *(s for s in dir(b) if not s.startswith("_")))  # образовалось еще одно поле newAnother
+
 
 # k = l = m = B() # создаст 3 ссылки на один и тот-же экземпляр класса
 
@@ -46,11 +55,16 @@ print("var внутри экземпляров класса:", k.var, l.var, m.v
 print("id var внутри экземпляров класса:", id(k.var), id(l.var), id(m.var))
 del k.var  # после удаления переменной в экземпляре класса она будет разрешаться за счет
 # переменной внутри пространства имен объявления класса
+# поле экземпляра убить можно, поле класса можно убить только через сам класс
 print("var внутри экземпляров класса:", k.var, l.var, m.var)
 print("id var внутри экземпляров класса:", id(k.var), id(l.var), id(m.var))
 
 
-print("\n\nCLASS A __INIT__")
+
+
+
+
+print("\n\nCLASS A __INIT__ илюстрация создания полей при инициализации экземпляра")
 class A:
     def __init__(self, start):
         print("инициализация полей в момент создания экземпляра класса")
