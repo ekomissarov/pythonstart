1dba789cf3a8f4b3855152afbaeabd91713a9073
eugene <eugene.komissarov@gmail.com>
2018-03-02 15:33:26 +0300

    upd1

---
+++ based_on_uneex/random1.py
@@ -0,0 +1,20 @@
+#[UNИX][Python] Лекция 3. Стандартные типы данных и выражения-конструкторы
+import random
+
+print("псевдослучайные числа")
+print(random.randrange(100))
+print(random.randrange(100))
+print(random.randrange(100))
+print()
+
+random.seed(1)
+print(random.randrange(100))
+print(random.randrange(100))
+print(random.randrange(100))
+print()
+
+random.seed(1)
+print(random.randrange(100))
+print(random.randrange(100))
+print(random.randrange(100))
+print()
