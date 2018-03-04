14bf518d16c6b9c1db0d60b2d24c8bbf3f379a1f
eugene <eugene.komissarov@gmail.com>
2018-03-04 23:44:27 +0300

    upd1

--- uneex_homework/14_look_and_say_Conway.py
+++ uneex_homework/14_look_and_say_Conway.py
@@ -12,20 +12,36 @@ Output:
 2
 '''
 
-l = [1]
-s = 1
-y=''
+def generat_conoway(seed):
+    yield seed
+    l = [int(seed)]
+    s = 1
+    y=''
 
-while len(l)<10:
-    x = str(l[len(l) - 1]) + 'Q'
-    for i in range(len(x) - 1):
-        if x[i] == x[i + 1]:
-            s += 1  # счетчик количества одинаковых цифр
-        else:  # как только цифры не совпадают, возвращаем счетчик к исходному состоянию сохраняем рез-т
-            y += str(s)+str(x[i])
-            s = 1  # для последнего значения "Q" счетчик возвращается в 1
+    while True:
+        x = str(l[len(l) - 1]) + 'Q'
+        for i in range(len(x) - 1):
+            if x[i] == x[i + 1]:
+                s += 1  # счетчик количества одинаковых цифр
+            else:  # как только цифры не совпадают, возвращаем счетчик к исходному состоянию сохраняем рез-т
+                y += str(s)+str(x[i])
+                yield s
+                yield x[i]
+                s = 1  # для последнего значения "Q" счетчик возвращается в 1
 
-    l.append(int(y))
-    y = ''
+        l.append(int(y))
+        y = ''
 
-print(l) # A005150
+    # print(l) # A005150
+
+
+N = int(input("Введите N: "))
+
+j = 0
+for i in generat_conoway(9):
+    N -= 1
+    if N < 0:
+        print("шаг {} цифра: {}".format(j, i))
+        break
+    j += 1
+
