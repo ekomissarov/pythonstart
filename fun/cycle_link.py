c1abdee5c9aaf08183eaf9426d445f6fb5bdd9cc
Eugene <eugene.komissarov@gmail.com>
2019-10-29 11:19:48 +0300

    Create cycle_link.py

---
+++ fun/cycle_link.py
@@ -0,0 +1,17 @@
+a=[1,2]
+b=[3,4]
+c=[5,6]
+a[0]=b
+b[0]=c
+c[0]=a
+del a[1]
+del b[1]
+del c[1]
+print(a, b, c)
+print(a[0], b[0], c[0])
+print(id(a), id(b), id(c))
+
+tmp = a[0]
+for i in range(10):
+    tmp = tmp[0]
+    print(i, tmp)
