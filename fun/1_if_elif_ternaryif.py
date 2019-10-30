7f2147d63bf730fbe2d39673bb5071b587e97d4b
Eugene <eugene.komissarov@gmail.com>
2019-10-30 11:23:40 +0300

    Create 1_if_elif_ternaryif.py

---
+++ fun/1_if_elif_ternaryif.py
@@ -0,0 +1,14 @@
+x = int(input("Please enter an integer: "))
+
+if x < 0:
+    x = 0
+    print('Negative changed to zero')
+elif x == 0:
+    print('Zero')
+elif x == 1:
+    print('Single')
+else:
+    print('More')
+
+x = 100 if x > 100 else x
+print("Set x not more than 100:", x)
