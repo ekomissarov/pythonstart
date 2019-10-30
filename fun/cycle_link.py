690b4f68b3b875f03d82fcb904e7377650d5c145
Eugene <eugene.komissarov@gmail.com>
2019-10-30 11:18:50 +0300

    Update cycle_link.py

--- fun/cycle_link.py
+++ fun/cycle_link.py
@@ -1,9 +1,9 @@
-a=[1,2]
-b=[3,4]
-c=[5,6]
-a[0]=b
-b[0]=c
-c[0]=a
+a = [1, 2]
+b = [3, 4]
+c = [5, 6]
+a[0] = b
+b[0] = c
+c[0] = a
 del a[1]
 del b[1]
 del c[1]
@@ -12,8 +12,8 @@ print(a[0], b[0], c[0])
 print(id(a), id(b), id(c))
 
 tmp = a[0]
-i=0
-while i<=10:
+i = 0
+while i <= 10:
     tmp = tmp[0]
     print(i, tmp, id(tmp))
-    i+=1
+    i += 1
