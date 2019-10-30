71e7f6b1719b79d33123766f226a62dec294a7ec
Eugene <eugene.komissarov@gmail.com>
2019-10-30 11:41:31 +0300

    Create 1_dir_id_help.py

---
+++ fun/1_dir_id_help.py
@@ -0,0 +1,19 @@
+print("Глобальное пространство имен:", dir())
+import math
+print("Глобальное пространство имен после импорта модуля math:", dir())
+print("Глобальное пространство имен модуля math:", dir(math))
+from math import sin
+print("Глобальное пространство имен после импорта модуля функции sin модуля math:", dir())
+print("Справка по функции sin")
+print(help(sin))
+
+a = 5
+print("Все в Python это объект, даже функция", type(a), type(sin), id(a), id(sin))
+a = sin
+print("Их можно связывать разными именами", type(a), type(sin), id(a), id(sin))
+print("a is sin:", a is sin)
+from sys import getrefcount
+print("Количество ссылок на объект можно посмотреть с помощью getrefcount", getrefcount(sin))
+
+print("Поля и методы объекта описаны в пространстве имен этого объекта", dir(sin))
+print("Объект функции отличается от просто объекта тем что в его пространстве имен есть метод __call__")
